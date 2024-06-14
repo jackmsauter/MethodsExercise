345c682e296bcefa0e80d49d473d27559c7b57af
jacksauter <jackmsauter99@gmail.com>
2024-06-14 12:41:34 -0400

    created madlib method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace MethodsExercise
+﻿
+static void MadLib()
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine("What is your name?");
+    string userName = Console.ReadLine();
+    Console.WriteLine($"Hello, {userName}! What is your fav wing flavor?");
+    string favWing = Console.ReadLine();
+    Console.WriteLine("Fav down & distance?");
+    string favDown = Console.ReadLine();
+    Console.WriteLine("Fav coverage?");
+    string favCoverage = Console.ReadLine();
+
+    string value = $"One day, {userName} was munching on some {favWing} wings when all of a sudden, the ghost of John Madden asked him what he would run on {favDown}. Then he asked why does he run so much {favCoverage}.";
+    Console.WriteLine(value);
 }
+MadLib();
